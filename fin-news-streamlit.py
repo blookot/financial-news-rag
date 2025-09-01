95d857151369fb169e06637eb82e012329f1b098
Vincent Maury <vincent.maury@elastic.co>
2025-09-01 18:23:45 +0200

    add ES q&a

--- fin-news-streamlit.py
+++ fin-news-streamlit.py
@@ -81,11 +81,19 @@ def search(query_text):
         link = source.get("link", "Lien non disponible")
         context += f"Titre: {title}\nTexte: {message}\nSource: {tags}\nLien: {link}\n\n---\n\n"
 
+    # Display in debug container
+    chat_debug_container.divider()
+    chat_debug_container.write(f"**Requête associée**")
+    chat_debug_container.write(query)
+    chat_debug_container.write(f"**Documents retournés**")
+    chat_debug_container.write(context)
+
+
     prompt = f"""
             Tu es un assistant d'analyse financière pour un trader expérimenté. Ta mission est de synthétiser les actualités financières fournies dans le CONTEXTE ci-dessous. Tu dois aller droit au but.
 
             ### CONSIGNES :
-            1.  **Synthèse concise** : dans ta réponse, commence par fournir un résumé des actualités les plus pertinentes en 100 mots. Puis, sous forme de liste à puces, liste tous les articles pertinents qui détaillent l'actualité.
+            1.  **Synthèse concise** : dans ta réponse, commence par fournir un résumé des actualités les plus pertinentes en 100 mots. Puis, sous forme de liste à puces, liste les 5 articles les plus pertinents qui détaillent l'actualité.
             2.  **Sources** : varie les sources autant que possible.
             3.  **Source cliquable** : Termine **chaque point** par le lien vers l'article original, formaté en Markdown comme ceci : `[nom de la source](lien url complet)`. Si le lien n'est pas disponible, essaie de l'extraire du message lui-même (qui se termine souvent par un lien URL)
             4.  **Langue** : La réponse doit être exclusivement en **Français**.
@@ -121,17 +129,28 @@ def chat_with_llm(prompt, query):
     return response.choices[0].message.content
 
 
-st.title("Actualités financières")
+st.header("Synthèse d'actualités financières")
+# imc = Image.open("favicon.png")
+st.set_page_config(
+    page_title="Actualités financières",
+    # layout="wide",
+    # page_icon=imc,
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': 'Cette application montre un usage d\'Elasticsearch pour du RAG'
+    }
+)
+st.write("[Doc](https://www.elastic.co/docs/solutions/search/rag)")
 
-# Main chat form
-with st.form("chat_form"):
-    query = st.text_input("Question : ")
-    submit_button = st.form_submit_button("Envoyer")
+with st.container():
+    chat_input_container = st.container()
+    chat_output_container= st.container()
+    chat_debug_container = st.container()
 
-# Generate and display response on form submission
-negResponse = "Désolé, je ne suis pas capable de vous répondre avec les informations que j'ai."
-if submit_button:
-    prompt = search(query)
-    answer = chat_with_llm(prompt, query)
-    
-    st.write(f"Réponse : {answer.strip()}")
+with chat_input_container:
+    if user_input:=st.chat_input("Question (taper Entrée pour valider)"):
+        with st.spinner("Processing..."):
+            prompt = search(user_input)
+            answer = chat_with_llm(prompt, user_input)
+            chat_output_container.write(f"Réponse : {answer.strip()}")
+
